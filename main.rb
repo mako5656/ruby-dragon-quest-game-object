db804504a3656aeab56e84bc8b4d7074c5989b68
Makoto Ikami <makoto.ikami@gmail.com>
2022-12-17 23:48:56 +0900

    モンスターの攻撃処理実装

--- main.rb
+++ main.rb
@@ -37,16 +37,53 @@ class Brave
 
 end
 
+
 class Monster
-  attr_reader :name, :offense, :defense
-  attr_accessor :hp
+  # nameを削除
+  attr_reader :offense, :defense
+  # nameを追加
+  attr_accessor :hp, :name
+
+  POWER_UP_RATE = 1.5
+  CALC_HALF_HP = 0.5
 
   def initialize(**params)
     @name = params[:name]
     @hp = params[:hp]
     @offense = params[:offense]
     @defense = params[:defense]
+
+    @transform_flag = false
+    @trigger_of_transform = params[:hp] * CALC_HALF_HP
   end
+
+  def attack(brave)
+    if @hp <= @trigger_of_transform && @transform_flag == false
+      @transform_flag = true
+      transform
+    end
+    puts "#{@name}の攻撃"
+
+    damage = @offense - brave.defense
+    brave.hp -= damage
+
+    puts "#{brave.name}は#{damage}のダメージを受けた"
+    puts "#{brave.name}の残りHPは#{brave.hp}だ"
+  end
+
+  private
+
+    def transform
+      transform_name = "ドラゴン"
+
+      puts <<~EOS
+      #{@name}は怒っている
+      #{@name}は#{transform_name}に変身した
+      EOS
+
+      @offense *= POWER_UP_RATE
+      @name = transform_name
+    end
 
 end
 
